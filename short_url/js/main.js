63cac6203e41ee11e08088e1936c0c7c1fd72e7e
Damon Windsor <damonwindsor@gmail.com>
2022-06-01 16:47:01 +1000

    90% complete
    [Updated]
    - Added about.html
      - Added Form for contact
    - Finalized Styling
    - Added Instructions
    
    [+] I still need to complete the Validation, it's buggy for some reason in its current state
    [+] I intend to use the images I added to show a Before, and After of the Long URL.

--- short_url/js/main.js
+++ short_url/js/main.js
@@ -1,8 +1,13 @@
+// Finding the Users input box within the HTML
 const searchBox = document.getElementById('searchBox');
+// Finding the Users Submit button within the HTML
 const searchButton = document.getElementById('searchButton');
+// Finding where we will place our results
 const results = document.getElementById('results');
 
+// Where we find our JSON data
 const baseUrl = "https://tinyurl.com/openapi/v2.json";
+// Our Token for the API
 const TOKEN = "Ul7iF9tRRjBmiiIW6xY39Adzgu12fRosT4sHqQoMtjDi5f4sFFXKjlUyjhYl";
 
 
@@ -30,7 +35,7 @@ function userUrl() {
 	create(input)
 
 }
-userUrl()
+
 
 
 // This uses the API to shorten the URL
@@ -42,15 +47,19 @@ async function create(longURL) {
 			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify({
+			// longURL is identified as the Original URL within the API
 			url: longURL,
 			domain: "tiny.one"
 		})
 	})
 	const data = await response.json()
-	document.getElementById("shortened").innerHTML = data.data.tiny_url;
+	const link = document.createElement("a")
+	link.href = data.data.tiny_url
+	link.innerHTML = data.data.tiny_url
+	document.getElementById("shortened").append(link)
 	console.log(data.data.tiny_url)
+	console.log(data)
 }
 
-// google is just a sample address, so you can see the output we desire in console
 
 
